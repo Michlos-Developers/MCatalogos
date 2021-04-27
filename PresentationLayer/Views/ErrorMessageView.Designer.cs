a44f5bed5fe08608393c5885988f0529e6ca367f
MichlosBSB <michlos@gmail.com>
2021-04-27 15:50:51 -0300

    MessageBox created.

---
+++ PresentationLayer/Views/ErrorMessageView.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace PresentationLayer.Views
+{
+    partial class ErrorMessageView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.BtnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(13, 13);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(291, 128);
+            this.messageTextBox.TabIndex = 0;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(229, 147);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.Text = "Fechar";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.Location = new System.Drawing.Point(148, 147);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.Size = new System.Drawing.Size(75, 23);
+            this.BtnCopy.TabIndex = 1;
+            this.BtnCopy.Text = "Copiar";
+            this.BtnCopy.UseVisualStyleBackColor = true;
+            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // ErrorMessageView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 179);
+            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.messageTextBox);
+            this.Name = "ErrorMessageView";
+            this.Text = "Error Message";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Button BtnCopy;
+    }
+}
---
+++ PresentationLayer/Views/ErrorMessageView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.Views
+{
+    public partial class ErrorMessageView : Form
+    {
+        public ErrorMessageView()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ShowErrorMessageView(string windowsTitle, string errorMessage)
+        {
+            this.Text = windowsTitle;
+            this.messageTextBox.Text = errorMessage;
+            this.ShowDialog();
+            this.Close();
+        }
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            if (messageTextBox.Text != "")
+            {
+                System.Windows.Forms.Clipboard.SetText(messageTextBox.Text);
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
